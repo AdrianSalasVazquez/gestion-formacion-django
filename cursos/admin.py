50bdffe8c98a7284570b67673ea6e2158b145f15
Adrian Salas <adriansalasvazquez@gmail.com>
2026-06-15 08:43:41 +0200

    Arreglado el filtro de curso en administración

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,6 +1,52 @@
 from django.contrib import admin
 from .models import Curso
 from django.utils.html import format_html
+from datetime import timedelta
+from django.utils import timezone
+
+class FiltroFechas(admin.SimpleListFilter):
+    title = "Filtro temporal"
+    parameter_name = "periodo"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("hoy", "Hoy"),
+            ("manana", "Mañana"),
+            ("semana", "Esta semana"),
+            ("mes", "Este mes"),
+            ("anio", "Este año"),
+        )
+
+    def queryset(self, request, queryset):
+        now = timezone.now()
+        hoy = now.date()
+
+        if self.value() == "hoy":
+            return queryset.filter(fecha_inicio=hoy)
+
+        if self.value() == "manana":
+            mañana = hoy + timedelta(days=1)
+            return queryset.filter(fecha_inicio=mañana)
+
+        if self.value() == "semana":
+            inicio = hoy - timedelta(days=hoy.weekday())
+            fin = inicio + timedelta(days=7)
+            return queryset.filter(fecha_inicio__gte=inicio, fecha_inicio__lt=fin)
+
+        if self.value() == "mes":
+            inicio = hoy.replace(day=1)
+            if hoy.month == 12:
+                fin = hoy.replace(year=hoy.year + 1, month=1, day=1)
+            else:
+                fin = hoy.replace(month=hoy.month + 1, day=1)
+            return queryset.filter(fecha_inicio__gte=inicio, fecha_inicio__lt=fin)
+
+        if self.value() == "anio":
+            inicio = hoy.replace(month=1, day=1)
+            fin = hoy.replace(year=hoy.year + 1, month=1, day=1)
+            return queryset.filter(fecha_inicio__gte=inicio, fecha_inicio__lt=fin)
+
+        return queryset
 
 # Register your models here.
 @admin.register(Curso)
@@ -22,12 +68,12 @@ class CursoAdmin(admin.ModelAdmin):
     )
     list_filter = (
         'activo',
-        'fecha_inicio',
+        'plazas',
+        FiltroFechas,
     )
     ordering = (
         'nombre',
     )
-    
     fieldsets = (
         (
             'Información General',
